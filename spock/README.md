4b3ff68ca6964ced294717c2b4892aaf167f1f73
Adam Laczynski <adam.laczynski@gmail.com>
2015-11-22 23:14:40 +0100

    spock

---
+++ spock/README.md
@@ -0,0 +1,4 @@
+# Doc
+* http://spockframework.github.io/spock/docs/1.0/index.html
+* https://github.com/spockframework/spock-example
+
---
+++ spock/src/test/groovy/org/asl/spock/SpockTest.groovy
@@ -0,0 +1,49 @@
+package org.asl.spock
+
+import spock.lang.Specification
+
+class SpockTest extends Specification {
+
+    /******************************************************************************************************************/
+    def 'shortest - one block - setup'() {
+        setup:
+        new ArrayList<>();
+    }
+
+    def 'shortest - one block - given, alias for setup'() {
+        given:
+        new ArrayList<>();
+    }
+
+    def 'shortest - one block - expect'() {
+        expect:
+        new ArrayList<>().isEmpty()
+    }
+
+    /******************************************************************************************************************/
+    def 'none block - this is not spock feature method'() {
+        new ArrayList().isEmpty()
+    }
+
+    /******************************************************************************************************************/
+    def 'when-then vs expect - use expect for purely functional method, no side effects'() {
+        expect:
+        Math.max(1, 2) == 2
+    }
+
+    def 'when-then vs expect - use given-then for function with side effects'() {
+        when:
+        def list = new ArrayList<>()
+        list.add("a")
+
+        then:
+        list.size() == 1
+    }
+
+    /******************************************************************************************************************/
+    def 'expect - can contain only conditions and variable definitions'() {
+        expect:
+        def expectedMax = 2
+        Math.max(1, 2) == expectedMax
+    }
+}
