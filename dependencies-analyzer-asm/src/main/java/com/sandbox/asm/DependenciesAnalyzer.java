5f49e6940a66645c02c486d760c23973874c8314
Adam Laczynski <adam.laczynski@gmail.com>
2016-09-26 00:23:04 +0200

    add dependencies analyzer

--- dependencies-analyzer-asm/src/main/java/com/sandbox/asm/DependenciesAnalyzer.java
+++ dependencies-analyzer-asm/src/main/java/com/sandbox/asm/DependenciesAnalyzer.java
@@ -9,27 +9,23 @@ import java.util.TreeSet;
 
 public class DependenciesAnalyzer {
 
-    public Set<String> findDependencies(String className, String include) {
+    public Set<String> findDependencies(String className, String include) throws IOException {
         Set<String> dependencies = new TreeSet<>();
         findDependencies(className, dependencies, include);
         dependencies.remove(className);
         return dependencies;
     }
 
-    private void findDependencies(String className, Set<String> dependencies, String classNamePrefix) {
-        try {
-            ClassReader reader = new ClassReader(className);
-            ClassesCollector collector = new ClassesCollector();
-            ClassRemapper visitor = new ClassRemapper(new ClassDependenciesVisitor(), collector);
-            reader.accept(visitor, 0);
-            for (String dependentClass : collector.classes()) {
-                if (dependentClass.startsWith(classNamePrefix) && !dependencies.contains(dependentClass)) {
-                    dependencies.add(dependentClass);
-                    findDependencies(dependentClass, dependencies, classNamePrefix);
-                }
+    private void findDependencies(String className, Set<String> dependencies, String classNamePrefix) throws IOException {
+        ClassReader reader = new ClassReader(className);
+        ClassesCollector collector = new ClassesCollector();
+        ClassRemapper visitor = new ClassRemapper(new ClassDependenciesVisitor(), collector);
+        reader.accept(visitor, 0);
+        for (String dependentClass : collector.classes()) {
+            if (dependentClass.startsWith(classNamePrefix) && !dependencies.contains(dependentClass)) {
+                dependencies.add(dependentClass);
+                findDependencies(dependentClass, dependencies, classNamePrefix);
             }
-        } catch (IOException e) {
-            System.err.println("class not found: " + className);
         }
     }
 }
